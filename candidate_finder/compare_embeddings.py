8196a3257c9f1e9864e3ee2b42e621380b3a1ce3
kasra-hosseini <khosseini@turing.ac.uk>
2020-05-29 15:03:52 +0000

    Prettify the output

--- candidate_finder/compare_embeddings.py
+++ candidate_finder/compare_embeddings.py
@@ -13,8 +13,9 @@ compare_mode = "l2"
 embed = torch.load(path2embedding)
 dataset = pd.read_csv(path2dataset, header=None, sep="\t")
 
+print(f"{'string to compare':>40s}, {'reference string':>40s}, {'l2_norm'}")
 for i in range(len(embed)):
     if compare_mode == "l2":
         l2_norm = torch.sum(torch.abs(embed[i] - embed[ref_id])**2)
-        print(f"{dataset.iloc[i][0]}, {dataset.iloc[ref_id][0]}, {l2_norm}")
+        print(f"{dataset.iloc[i][0]:>40s}, {dataset.iloc[ref_id][0]:>40s}, {l2_norm}")
 
