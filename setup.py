f2dd13dccd8abc615100f2db316563af9195fb4c
kasra-hosseini <khosseini@turing.ac.uk>
2020-09-15 15:08:36 +0100

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="DeezyMatch",
-    version="1.1.0",
+    version="1.2.0",
     description="A Flexible Deep Learning Approach to Fuzzy String Matching",
     author=u"The LwM Development Team",
     #author_email="",
